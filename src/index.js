b262b4ae57b200cd65200de7b5af6be7274f2502
Pulkit Mehta <42738169+MehtaPulkit@users.noreply.github.com>
2020-04-27 00:17:02 +1000

    Setup done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/layout/App';
+const Lib=()=>{
+    return(
+        <div>
+            <App/>
+            </div>
+    )
+}
+ReactDOM.render(<Lib/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
