677a69e14390119d4b82d5f61697486813a84ded
Pulkit Mehta <42738169+MehtaPulkit@users.noreply.github.com>
2020-05-10 17:51:07 +1000

    CRUD

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import EventDashboard from '../../features/event/eventDashboard/EventDashboard'
-import NavBar from '../../features/nav/navbar/NavBar';
-import { Container } from 'semantic-ui-react';
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navbar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
--- src/features/event/eventForm/EventForm.js
+++ src/features/event/eventForm/EventForm.js
@@ -9,18 +9,32 @@ class EventForm extends Component {
     city: "",
     hostedBy: "",
   };
+
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent,
+      });
+    }
+  }
+
   handleFormSubmit = (evt) => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if (this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   };
   handleInputChange = (evt) => {
     this.setState({
       [evt.target.name]: evt.target.value,
     });
   };
+
   render() {
     const { cancelFormOpen } = this.props;
-    const { title, date, venue, city, hostedBy } = this.props;
+    const { title, date, venue, city, hostedBy } = this.state;
     return (
       <Segment>
         <Form onSubmit={this.handleFormSubmit}>
