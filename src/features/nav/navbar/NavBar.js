25aeaaa0599725d76c4003b0152d2d01a4ddb6a0
Pulkit Mehta <42738169+MehtaPulkit@users.noreply.github.com>
2020-05-14 00:40:57 +1000

    Routing done

--- src/features/nav/navbar/NavBar.js
+++ src/features/nav/navbar/NavBar.js
@@ -1,29 +1,55 @@
-import React, { Component } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import SignedOutMenu from "../menus/SignedOutMenu";
+import SignedInMenu from "../menus/SignedInMenu";
 
 class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item header>
-                    <img src="assets/logo.png" alt="logo" />
-                    Re-vents
-                    </Menu.Item>
-                    <Menu.Item name="Events" />
-                    <Menu.Item>
-                    <Button floated="right" positive inverted content="Create Event" />
-                    </Menu.Item>
-                    <Menu.Item position="right">
-                    <Button basic inverted content="Login" />
-                    <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
-                    </Menu.Item>
-                </Container>
-                </Menu>
-            </div>
-        )
-    }
+  state = {
+    authenticated: true,
+  };
+
+  handleSignIn = () => {
+    this.setState({ authenticated: true });
+  };
+  handleSignOut = () => {
+    this.setState({ authenticated: false });
+    this.props.history.push("/");
+  };
+
+  render() {
+    const { authenticated } = this.state;
+    return (
+      <div>
+        <Menu inverted fixed="top">
+          <Container>
+            <Menu.Item as={NavLink} exact to="/" header>
+              <img src="assets/logo.png" alt="logo" />
+              Re-vents
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/events" name="Events" />
+            <Menu.Item as={NavLink} to="/people" name="People" />
+            <Menu.Item>
+              <Button
+                as={Link}
+                to="/createEvent"
+                floated="right"
+                positive
+                inverted
+                content="Create Event"
+              />
+            </Menu.Item>
+            {authenticated ? (
+              <SignedInMenu signOut={this.handleSignOut} />
+            ) : (
+              <SignedOutMenu signIn={this.handleSignIn} />
+            )}
+          </Container>
+        </Menu>
+      </div>
+    );
+  }
 }
 
-export default NavBar;
+export default withRouter(NavBar)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
+import "./index.css";
+import App from "./app/layout/App";
+import { BrowserRouter } from "react-router-dom";
 
-
-const Lib=()=>{
-    return(
-        <div>
-            <App/>
-            </div>
-    )
-}
-ReactDOM.render(<Lib/>, document.getElementById('root'));
+const Lib = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </div>
+  );
+};
+ReactDOM.render(<Lib />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
